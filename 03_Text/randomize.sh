1082a1a4c5a640ca637efd9c12ec0beae7cbd85a
dannyp <s02210506@gse.cs.msu.ru>
2025-10-06 23:48:59 +0300

    [task 03] done

--- 03_Text/randomize.sh
+++ 03_Text/randomize.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-delay=${1:-0.2}
+delay=${1:-0.05}
 
 i=0
 j=0
-chars=( )
-while IFS= read -r -n1 char ; do
-    case $char in
-        $'\n')
-            i=$((i+1))
-            j=0
-            ;;
-        *)
-            chars+=("$i $j $(printf "%s" "$char" | od -v -An -tx1)"$'\n')
-            j=$((j+1))
-            ;;
-    esac    
+while IFS='' read -r -n1 char ; do
+    if [ -z "$char" ] ; then
+        i=$((i+1))
+        j=0
+    else
+        if [ "$char" != ' ' ] ; then
+            chars+=("$i $j$(printf "%s" "$char" | od -v -An -tx1)")
+        fi
+        j=$((j+1))
+    fi
 done
 
+tput clear
 
-echo ${chars[@]} #| shuf | while read -r x y c ; do
+{ for elem in "${chars[@]}"; do
+    echo "$elem"
+done } | shuf | { while read -r x y c ; do
+    tput cup $x $y
+    printf "\x$c"
+    sleep $delay
+done }
+
+tput cup $i 0
